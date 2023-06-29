850ddd94ca6e3e8fc196b245dce757af1a7bf335
Ouedraogo <aminata24ouedraogo@gmail.com>
2023-06-29 15:22:58 +0200

    ami commit

---
+++ php/home.php
@@ -0,0 +1,26 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>DevCoder</title>
+    <link rel="stylesheet" type="text/css" href="../style/style.css">
+    <link rel="shortcut icon" href="images/logo.png" type="image/x-icon">
+</head>
+<body>
+    <div class="container">
+        <img src="../images/logo.png" alt="Logo de la plateforme">
+        
+             <h5 class="slogan"> Building a stronger Developper Community Together</h5>  
+        <p class="description">Bienvenue sur notre plateforme dédiée aux développeurs. <br>
+             Rejoignez notre communauté pour échanger, partager vos connaissances  <br>
+             et résoudre les défis de développement web.</p>
+
+        <div class="button-group">
+            <a href="inscription.php" class="button">Inscription</a>
+            <a href="connexion.php" class="button">Connexion</a>
+        </div>
+    </div>
+</body>
+</html>
+
